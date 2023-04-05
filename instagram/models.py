73f241bae56e7eee1fff310041a9ab730e1d8bab
fydnrl23 <fydnrl23@gmail.com>
2023-04-06 00:33:29 +0900

    처음

---
+++ instagram/models.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+from django.db import models
+# from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.TextField()
+    photo = models.ImageField(blank=True, upload_to='instagram/post/%Y%m%d')
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tag_set = models.ManyToManyField('Tag',blank=True)
+
+    # Java의 toSting
+    def __str__(self):
+        #return f"Custom Post object ({self.id})"
+        #return "Custom Post object ({})".format(self.id)
+        return self.message
+    
+    class Meta:
+        ordering = ['-id']
+    
+    # def message_length(self):
+    #     return len(self.message)
+    # message_length.short_description = '메세지 글자수'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    #post_set = models.ManyToManyField(Post)
+    
+    def __str__(self):
+        return self.name
